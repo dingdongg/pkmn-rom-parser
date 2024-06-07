16b892d0c08c3ac833d2b332808f2bb41b8e88d8
dingdongg <cs.dg.kim@gmail.com>
2024-06-07 02:28:11 -0600

    add more constants

--- consts/consts.go
+++ consts/consts.go
@@ -3,3 +3,22 @@ package consts
 const BLOCK_SIZE_BYTES = 32
 const PARTY_POKEMON_SIZE = 236
 const PERSONALITY_OFFSET = 0xA0
+
+const (
+	BLOCK_A_ITEM = 0x2
+	BLOCK_A_ABILITY = 0xD
+	BLOCK_A_EV = 0x10
+)
+
+const (
+	BLOCK_B_IV = 0x10
+)
+
+const (
+	BLOCK_C_NICKNAME = 0x0
+)
+
+const (
+	BATTLE_STATS_LEVEL = 0x4
+	BATTLE_STATS_STAT = 0x8
+)
