8fa3cc7ee2293fe12f965384b72e17e9f8ec976f
dingdongg <cs.dg.kim@gmail.com>
2024-06-16 16:30:43 -0600

    use new testutil templates package, create more tests

--- rom_writer/req/req_test.go
+++ rom_writer/req/req_test.go
@@ -1,20 +1,23 @@
 package req
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/dingdongg/pkmn-rom-parser/v5/tutil"
 )
 
+var templates = tutil.GetTemplates()
+
 func TestNewWriteRequest(t *testing.T) {
 	wr := NewWriteRequest(0)
 
 	if wr.PartyIndex != 0 {
-		tutil.ErrUint(t, 0, wr.PartyIndex, false)
+		t.Fatalf(templates.Uint, 0, wr.PartyIndex)
 	}
 
 	if len(wr.Contents) != 0 {
-		tutil.ErrInt(t, 0, len(wr.Contents), false)
+		t.Fatalf(templates.Int, 0, len(wr.Contents))
 	}
 }
 
@@ -25,22 +28,26 @@ func TestWriteItem(t *testing.T) {
 
 	res, ok := wr.Contents[ITEM]
 	if !ok {
-		tutil.ErrMsg(t, "expected map entry to exist, but got DNE")
+		t.Fatal("expected map entry to exist, but got DNE")
 	}
+
+	t.Log(res)
 
 	byteForm, err := res.Bytes()
 	if err != nil {
-		tutil.ErrMsg(t, "byte conversion failed")
+		t.Fatal("byte conversion failed")
 	}
+
+	t.Log(byteForm)
 
 	// item ID should take up 2 bytes only
 	if len(byteForm) != 2 {
-		tutil.ErrInt(t, 2, len(byteForm), false)
+		t.Fatalf(templates.Int, 2, len(byteForm))
 	}
 
 	id := uint16(byteForm[0]) | (uint16(byteForm[1]) << 8)
 	if id != 0x0001 {
-		tutil.ErrUint(t, 0x0001, uint(id), true)
+		t.Fatalf(templates.UintHex, 0x0001, id)
 	}
 }
 
@@ -51,19 +58,49 @@ func TestWriteAbility(t *testing.T) {
 
 	res, ok := wr.Contents[ABILITY]
 	if !ok {
-		tutil.ErrMsg(t, "expected map entry to exist, but got DNE")
+		t.Fatal("expected map entry to exist, but got DNE")
 	}
 
 	byteForm, err := res.Bytes()
 	if err != nil {
-		tutil.ErrMsg(t, "byte conversion failed")
+		t.Fatal("byte conversion failed")
 	}
 
 	if len(byteForm) != 1 {
-		tutil.ErrInt(t, 1, len(byteForm), false)
+		t.Fatalf(templates.Int, 1, len(byteForm))
 	}
 
 	if byteForm[0] != 26 {
-		tutil.ErrUint(t, 26, uint(byteForm[0]), true)
+		t.Fatal(templates.UintHex, 26, byteForm[0])
 	}
 }
+
+func TestWriteBattleStats(t *testing.T) {
+	wr := NewWriteRequest(0)
+	stats := [6]uint{65535, 0, 124, 7000, 333, 255}
+	wr.WriteBattleStats(stats[0], stats[1], stats[2], stats[4], stats[5], stats[3])
+	
+	res, ok := wr.Contents[BATTLE_STATS]
+
+	if !ok {
+		t.Fatal("expected map entry to exeist, but got DNE")
+	}
+
+	byteForm, err := res.Bytes()
+	if err != nil {
+		t.Fatal("byte conversion failed")
+	}
+
+	if len(byteForm) != 12 {
+		t.Fatalf(templates.Int, 12, len(byteForm))
+	}
+
+	for i := 0; i < len(byteForm); i += 2 {
+		t.Logf("index %d\n", i)
+		expected := stats[i]
+		actual := binary.LittleEndian.Uint16(byteForm[i : i+2])
+		if actual != uint16(expected) {
+			t.Fatalf(templates.UintHex, expected, actual)
+		}
+	}
+}	
