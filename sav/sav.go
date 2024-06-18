3c6a3199c9e3eeb233d5b5ca56b837179247ec29
dingdongg <cs.dg.kim@gmail.com>
2024-06-18 16:41:22 -0600

    create common savefile interface

--- sav/sav.go
+++ sav/sav.go
@@ -1,17 +1,25 @@
 package sav
 
-import "github.com/dingdongg/pkmn-rom-parser/v7/consts/gamever"
+import (
+	"github.com/dingdongg/pkmn-rom-parser/v7/consts/gamever"
+	"github.com/dingdongg/pkmn-rom-parser/v7/validator"
+)
+
+type ISave interface {
+	GetChunk(offset uint) validator.Chunk
+	Validate() error
+}
 
 // tODO: include important offsets as fields
 type savPLAT struct {
-	Version gamever.GameVer
-	Data []byte
+	version gamever.GameVer
+	data []byte
 }
 
 // tODO: include important offsets as fields
 type savHGSS struct {
-	Version gamever.GameVer
-	Data []byte
+	version gamever.GameVer
+	data []byte
 }
 
 func NewSavPLAT(savefile []byte) *savPLAT {
@@ -21,9 +29,25 @@ func NewSavPLAT(savefile []byte) *savPLAT {
 	}
 }
 
+func (s *savPLAT) GetChunk(offset uint) validator.Chunk {
+	return validator.Chunk{}
+}
+
+func (s *savPLAT) Validate() error {
+	return nil
+}
+
 func NewSavHGSS(savefile []byte) *savHGSS {
 	return &savHGSS{
 		gamever.HGSS,
 		savefile,
 	}
+}
+
+func (s *savHGSS) GetChunk(offset uint) validator.Chunk {
+	return validator.Chunk{}
+}
+
+func (s *savHGSS) Validate() error {
+	return nil
 }
