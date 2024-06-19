515e9510caa9e7293cec07bd9d3188079796cacb
dingdongg <cs.dg.kim@gmail.com>
2024-06-19 01:01:13 -0600

    update type comments

--- rom_writer/req/types.go
+++ rom_writer/req/types.go
@@ -4,7 +4,7 @@ type Writable interface {
 	Bytes() ([]byte, error)
 }
 
-// for battle stats
+// for battle stats. implements Writable
 type WriteStats struct {
 	Hp        uint
 	Attack    uint
@@ -14,44 +14,25 @@ type WriteStats struct {
 	Speed     uint
 }
 
-type WriteEffortValue struct {
-	Hp uint
-	Attack uint
-	Defense uint
-	SpAttack uint
-	SpDefense uint
-	Speed uint
-}
+// implements Writable
+type WriteEffortValue WriteStats
 
-type WriteIndivValue struct {
-	Hp uint
-	Attack uint
-	Defense uint
-	SpAttack uint
-	SpDefense uint
-	Speed uint
-}
+// implements Writable
 
-// for IDs/level
+type WriteIndivValue WriteStats
+
+// for IDs/level. implements Writable
 type WriteUint struct {
 	Val uint
 	NumBytes uint	// number of bytes. Used in Bytes() implementation
 }
 
-// for nicknames
+// for nicknames. implements Writable
 type WriteString struct {
 	Val string
 }
 
 type NewData map[string]Writable
-
-// types that implement CompressibleStat can have their stat values
-// compressed into an unsigned integer. For instance,
-// each IV stat uses 5 bits (=30) - so IVs can be packed in a uint32.
-// each EV stat uses 8 bits (=48) - so EVs can be packed in a uint64.
-type CompressibleStat interface {
-	Compress(elemBits uint) uint
-}
 
 type WriteRequest struct {
 	PartyIndex uint
