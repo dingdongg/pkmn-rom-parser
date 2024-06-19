0f39ed49b066740ac54a418856df148073ed9fa8
dingdongg <cs.dg.kim@gmail.com>
2024-06-19 00:22:03 -0600

    use new ISave interface

--- parser.go
+++ parser.go
@@ -1,32 +1,28 @@
 package parser
 
 import (
-	"github.com/dingdongg/pkmn-rom-parser/v7/consts"
 	"github.com/dingdongg/pkmn-rom-parser/v7/rom_reader"
 	"github.com/dingdongg/pkmn-rom-parser/v7/rom_writer"
 	"github.com/dingdongg/pkmn-rom-parser/v7/rom_writer/req"
-	"github.com/dingdongg/pkmn-rom-parser/v7/validator"
-	"github.com/dingdongg/pkmn-rom-parser/v7/validator/locator"
+	"github.com/dingdongg/pkmn-rom-parser/v7/sav"
 )
 
 func Parse(savefile []byte) ([]rom_reader.Pokemon, error) {
-	if err := validator.Validate(savefile); err != nil {
+	game, err := sav.Validate(savefile)
+	if err != nil {
 		return []rom_reader.Pokemon{}, err
 	}
-	// TODO: make ISave implement this method,
-	// then call this method regardless of specific game version
-	chunk := locator.GetLatestSaveChunk(savefile)
-	// fmt.Println(*chunk)
-	partyData := chunk.SmallBlock.BlockData[consts.PERSONALITY_OFFSET_HGSS:]
 
+	partyData := game.GetPartySection()
 	return rom_reader.GetPartyPokemon(partyData), nil
 }
 
 func Write(savefile []byte, newBytes []req.WriteRequest) ([]byte, error) {
-	if err := validator.Validate(savefile); err != nil {
+	game, err := sav.Validate(savefile)
+	if err != nil {
 		return []byte{}, err
 	}
 
-	chunk := locator.GetLatestSaveChunk(savefile)
+	chunk := game.GetLatestData()
 	return rom_writer.UpdatePartyPokemon(savefile, *chunk, newBytes)
 }
