14d95c40048d1810d4810c45d6dc5b092b1e5274
dingdongg <donggyukimm@gmail.com>
2025-03-29 11:12:32 -0700

    impl pokemon struct pretty printing

--- revamp/models/models.go
+++ revamp/models/models.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/dingdongg/pkmn-rom-parser/v7/revamp/enums"
+import (
+	"fmt"
+
+	"github.com/dingdongg/pkmn-rom-parser/v7/revamp/enums"
+)
 
 type TODO struct{}
 
@@ -20,3 +24,47 @@ type Pokemon struct {
 	Gender    enums.Gender
 	Form      TODO
 }
+
+func (p *Pokemon) String() string {
+	ret := "-----\n"
+	ret += fmt.Sprintf("Name:        '%s'\n", p.Name)
+	ret += fmt.Sprintf("Level:       %d\n", p.Level)
+	ret += fmt.Sprintf("Exp. points: %d\n", p.Exp)
+	ret += fmt.Sprintf("Pokedex ID:  %d\n", p.PokedexId)
+	ret += fmt.Sprintf("Nature:      %d\n", p.Nature) // turn this into string for logging purposes
+	ret += fmt.Sprintf("Ability:     '%s'\n", p.Ability)
+	ret += fmt.Sprintf("Held Item:   '%s'\n", p.HeldItem)
+	ret += fmt.Sprintf("Gender:      %d\n", p.Gender)
+	ret += "-----\n"
+	return ret
+}
+
+/*
+
+A:
+- pokedex id
+- held item
+- ability
+- EV
+- EXP
+
+B:
+- moveset + PP
+- IV
+- gender
+- forms
+
+C:
+- name
+
+D:
+- none
+
+battle stats;
+- level
+- battle stats
+
+
+from DB:
+- base stats
+*/
