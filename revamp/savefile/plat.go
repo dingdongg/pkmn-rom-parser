5174edd35096c3e0653c2aa8775bfee7a80cd124
dingdongg <donggyukimm@gmail.com>
2025-03-29 23:19:29 -0700

    parse more fields

--- revamp/savefile/plat.go
+++ revamp/savefile/plat.go
@@ -3,8 +3,10 @@ package savefile
 import (
 	"encoding/binary"
 	"fmt"
+	"log"
 	"strings"
 
+	"github.com/dingdongg/pkmn-rom-parser/v7/char"
 	"github.com/dingdongg/pkmn-rom-parser/v7/crypt"
 	"github.com/dingdongg/pkmn-rom-parser/v7/data"
 	"github.com/dingdongg/pkmn-rom-parser/v7/revamp/enums"
@@ -62,21 +64,75 @@ func (pt *PlatSavefile) parsePokemon(index int) models.Pokemon {
 	raw := crypt.DecryptPokemon(pt.rawBytes[offset : offset+236])
 
 	blocks := shuffler.GetPokemonBlocks(raw)
-	a := blocks[0]
+	a, b, c := blocks[0], blocks[1], blocks[2]
 	printBlock(a, 16)
+
+	rawName := c[:0x16]
+	name := ""
+	for i := 0; i < len(rawName); i += 2 {
+		code := u16(rawName, i)
+		if code == char.END_OF_STRING {
+			break
+		}
+		chr, err := char.Char(code)
+		if err != nil {
+			log.Fatal(err)
+		}
+		name += chr
+	}
 
 	battleStats := raw[0x88 : 0x88+0x64]
 
 	ability, _ := data.GetAbility(uint(u8(a, 0xD)))
 	item, _ := data.GetItem(u16(a, 0x2))
 
+	ivBuffer := u32(b, 0x10)
+	getIv := func(statIndex int) uint8 {
+		val := (ivBuffer >> (5*statIndex)) & 0x1F
+		return uint8(val)
+	}
+
+	genderByte := u8(b, 0x18)
+	gender := enums.Male
+	if genderByte & 0x2 != 0 {
+		gender = enums.Female
+	} else if genderByte & 0x4 != 0 {
+		gender = enums.Unknown
+	}
+
 	return models.Pokemon{
+		Name: name,
 		PokedexId: u16(a, 0x0),
 		Exp: u32(a, 0x8),
 		Level: u8(battleStats, 0x4),
 		Nature: enums.Nature(u32(raw, 0) % 25),
 		Ability: ability,
 		HeldItem: item.Name,
+		Gender: gender,
+		EV: models.Stat[uint8]{
+			Hp: u8(a, 0x10),
+			Attack: u8(a, 0x11),
+			Defense: u8(a, 0x12),
+			SpeAttack: u8(a, 0x14),
+			SpeDefense: u8(a, 0x15),
+			Speed: u8(a, 0x13),
+		},
+		IV: models.Stat[uint8]{
+			Hp: getIv(0),
+			Attack: getIv(1),
+			Defense: getIv(2),
+			SpeAttack: getIv(4),
+			SpeDefense: getIv(5),
+			Speed: getIv(3),
+		},
+		Battle: models.Stat[uint16]{
+			Hp: u16(battleStats, 0x8),
+			Attack: u16(battleStats, 0xA),
+			Defense: u16(battleStats, 0xC),
+			SpeAttack: u16(battleStats, 0x10),
+			SpeDefense: u16(battleStats, 0x12),
+			Speed: u16(battleStats, 0xE),
+		},
 	}
 }
 
